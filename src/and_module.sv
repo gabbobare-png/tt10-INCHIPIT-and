ebea5c0cf46664c336ada079197fbfe2ea3c99b7
gabbobare-png <gabbobare@gmail.com>
2026-02-11 11:40:44 +0100

    Rename module from or_module to and_module

--- src/and_module.sv
+++ src/and_module.sv
@@ -1,7 +1,7 @@
-//  Module: or_module
+//  Module: and_module
 //
 
-module or_module
+module and_module
     (
         input logic a,
         input logic b,
@@ -9,4 +9,4 @@ module or_module
     );
 
     assign y = a | b;
-endmodule: or_module
+endmodule: and_module
